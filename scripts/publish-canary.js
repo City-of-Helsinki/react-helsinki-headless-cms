588870f57bd003fc61cfa065c5b75661844c9ca4
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2022-03-14 08:52:31 +0200

    Add package deployment workflows

---
+++ scripts/publish-canary.js
@@ -0,0 +1,42 @@
+const { exec } = require("child_process");
+
+const editJsonFile = require("edit-json-file");
+const currentGitCommit = require("git-rev-sync");
+
+function execShellCommand(cmd) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve([stdout, stderr]);
+      }
+    });
+  });
+}
+
+async function publishCanary() {
+  const file = editJsonFile(`${__dirname}/../package.json`, {
+    autosave: true,
+  });
+  const originalVersion = file.get("version");
+  const canaryVersion = `canary-${currentGitCommit.short()}`;
+
+  file.set("version", canaryVersion);
+
+  try {
+    const [stdout, stderr] = await execShellCommand(
+      "npm publish --tag canary --color always --registry=https://registry.npmjs.org/"
+    );
+
+    process.stdout.write(stdout);
+    process.stderr.write(stderr);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+  } finally {
+    file.set("version", originalVersion);
+  }
+}
+
+publishCanary();
