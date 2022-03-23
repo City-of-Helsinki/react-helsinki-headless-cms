61ea4f967d898ca0bb0574d49fa4436b4fbf2ff1
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2022-03-23 13:31:36 +0200

    Add NextJS utilities for navigation

---
+++ src/nextjs/getLanguageStaticProps.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ApolloClient, NormalizedCacheObject } from "@apollo/client";
+
+import {
+  LanguagesQuery,
+  LanguagesQueryVariables,
+  LanguagesDocument,
+} from "../common/headlessService/languages";
+
+export default async function getLanguageStaticProps(
+  apolloClient: ApolloClient<NormalizedCacheObject>
+) {
+  return apolloClient.query<LanguagesQuery, LanguagesQueryVariables>({
+    query: LanguagesDocument,
+  });
+}
