40ce124500b2cf12263872f11a064c693bd24dc5
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2022-03-29 13:24:58 +0300

    Export PageType

--- src/common/headlessService/types.ts
+++ src/common/headlessService/types.ts
@@ -1,6 +1,7 @@
 import { MenuQuery, PageQuery } from "./__generated__";
 
 export type Menu = MenuQuery["menu"];
+export type PageType = PageQuery["page"];
 export type SidebarContent = PageQuery["page"]["translation"]["sidebar"];
 
 export type {
