744d380fcff121821e2d1307bca59f3272ff7179
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2022-03-29 12:59:46 +0300

    Export graphql documents for testing

--- src/apollo/index.ts
+++ src/apollo/index.ts
@@ -1,2 +1,5 @@
 export { default as Navigation } from "./navigation/Navigation";
 export { default as PageContent } from "./pageContent/PageContent";
+export { PageDocument } from "../common/headlessService/page";
+export { MenuDocument } from "../common/headlessService/menu";
+export { LanguagesDocument } from "../common/headlessService/languages";
