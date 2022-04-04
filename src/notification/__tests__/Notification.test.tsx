4fd8d3f237c45fa17cf23eaf991bd142d832b441
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2022-04-04 12:52:38 +0300

    Add unit tests for the notification component

---
+++ src/notification/__tests__/Notification.test.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+import {
+  render,
+  screen,
+  userEvent,
+  waitFor,
+} from "../../common/utils/testingLibrary";
+import Notification from "../Notification";
+import notificationMock from "../__mocks__/notification.mock";
+
+test("should render notification with expected content", () => {
+  render(<Notification notification={notificationMock} />);
+
+  expect(
+    screen.getByRole("heading", { level: 2, name: notificationMock.title })
+  ).toBeInTheDocument();
+  expect(
+    screen.getByText(
+      notificationMock.content.replace("<p>", "").replace("</p>", "")
+    )
+  ).toBeInTheDocument();
+
+  const link = screen.getByRole("link", {
+    name: `${notificationMock.linkText}. Opens in a new tab. Opens a different website.`,
+  });
+
+  expect(link).toBeInTheDocument();
+  expect(link).toHaveAttribute("href", notificationMock.linkUrl);
+});
+
+test("should not render notification when it has expired", () => {
+  const dateInThePast = new Date();
+  dateInThePast.setDate(dateInThePast.getDate() - 1);
+
+  render(
+    <Notification
+      notification={{
+        ...notificationMock,
+        endDate: dateInThePast.toJSON(),
+      }}
+    />
+  );
+
+  expect(
+    screen.queryByRole("heading", { level: 2, name: notificationMock.title })
+  ).toEqual(null);
+});
+
+test("should not render notification when it has been dismissed", async () => {
+  render(<Notification notification={notificationMock} />);
+
+  expect(
+    screen.getByRole("heading", { level: 2, name: notificationMock.title })
+  ).toBeInTheDocument();
+
+  userEvent.click(
+    screen.getByRole("button", {
+      name: "close",
+    })
+  );
+
+  await waitFor(() =>
+    expect(
+      screen.queryByRole("heading", { level: 2, name: notificationMock.title })
+    ).toEqual(null)
+  );
+});
