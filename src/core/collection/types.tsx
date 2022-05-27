fbd989cd89ce6070092625ac71da054888f74416
Niko Lindroos <niko.j.makela@gmail.com>
2022-05-27 18:10:44 +0300

    Fix the CollectionItemType

--- src/core/collection/types.tsx
+++ src/core/collection/types.tsx
@@ -1,9 +1,7 @@
-import { LayoutArticle, LayoutPage } from "../../common/headlessService/types";
+import { ArticleType, PageType } from "../../common/headlessService/types";
 
 // TODO: HCRC-13 - Support also the event selection and the event search modules
-export type CollectionItemType =
-  | LayoutArticle["articles"][number]
-  | LayoutPage["pages"][number];
+export type CollectionItemType = ArticleType | PageType;
 
 export type CollectionType = {
   id?: string;
