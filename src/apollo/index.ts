bffc02697af8454e6f202589be412492a48d9eed
Ivan Melnik <melnik.job@gmail.com>
2022-07-09 16:47:07 +0300

    fix

--- src/apollo/index.ts
+++ src/apollo/index.ts
@@ -9,4 +9,7 @@ export * from '../common/headlessService/articles';
 export * from '../common/headlessService/landingPage';
 export * from '../common/headlessService/pageByTemplate';
 export { MenuDocument } from '../common/headlessService/menu';
-export { LanguagesDocument } from '../common/headlessService/languages';
+export {
+  LanguagesDocument,
+  LanguageCodeEnum,
+} from '../common/headlessService/languages';
