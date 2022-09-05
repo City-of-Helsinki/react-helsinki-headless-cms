fd4260bfebebcd9f07e13bdafc639244b0f8bcf5
Niko Lindroos <niko.j.makela@gmail.com>
2022-09-05 17:38:12 +0300

    Remove the LinkedEvents endpoint validation for app middleware routing
    HH-120

--- src/core/collection/Collection.tsx
+++ src/core/collection/Collection.tsx
@@ -22,7 +22,6 @@ import useEventsApolloClientFromConfig from '../configProvider/useEventsApolloCl
 import { getCollectionCards } from '../pageContent/utils';
 import { Config } from '../configProvider/configContext';
 import normalizeKeys from '../../linkedEvents/utils/normalizeKeys';
-import { LINKED_EVENTS_ENDPOINT } from '../../constants';
 import { ModuleItemTypeEnum } from '../../common/headlessService/constants';
 import { Link } from '../link/Link';
 
@@ -206,11 +205,9 @@ export function EventSearchCollection({
   // initAmountOfEvents only in case if load more is implemented (this feature is skipped now)
   const pageSize = 9; // collection.initAmountOfEvents
 
-  if (!url.startsWith(LINKED_EVENTS_ENDPOINT)) {
-    throw new Error('Illegal LinkedEvents origin set!');
-  }
-
-  const { searchParams } = new URL(url);
+  const searchParams = new URLSearchParams(
+    url.split('?')[1] ?? url.split('?')[0],
+  );
   const params = Object.fromEntries(searchParams.entries());
   const variables = {
     ...normalizeKeys(params),
