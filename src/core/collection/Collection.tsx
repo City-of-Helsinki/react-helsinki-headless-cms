f741868b8acd9e07cfd279911779321376fa3e43
Niko Lindroos <niko.j.makela@gmail.com>
2022-09-14 09:44:54 +0300

    Export getEventCollectionCards
    HH-83

--- src/core/collection/Collection.tsx
+++ src/core/collection/Collection.tsx
@@ -159,7 +159,7 @@ export function Collection({
   );
 }
 
-function getEventCollectionCards(
+export function getEventCollectionCards(
   collection: EventSelectionCollectionType | EventSearchCollectionType,
   items: EventType[],
   getRoutedInternalHref: Config['utils']['getRoutedInternalHref'],
