63c0c6b6bf21b203e1fd1ae70615d3a0d7f12c52
Niko Lindroos <niko.j.makela@gmail.com>
2023-02-03 15:35:45 +0200

    Show only available events in the event selection collection
    TH-1297

--- src/core/collection/utils.ts
+++ src/core/collection/utils.ts
@@ -1,3 +1,10 @@
+import isPast from 'date-fns/isPast';
+
+import { EventType } from '../../common/eventsService/types';
+
 export function getVenueIds(ids: number[]): string[] {
   return ids.map((id) => `tprek:${id}`);
 }
+
+export const isEventClosed = (event: EventType): boolean =>
+  !!event?.endTime && isPast(new Date(event.endTime));
