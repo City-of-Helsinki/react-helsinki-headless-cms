0c5c57e54cf6cdbb21f2e452bee12c6a33dab16d
Ivan Melnik <melnik.job@gmail.com>
2023-09-09 12:26:37 +0300

    fix: html content of the modules

--- src/core/utils/string.ts
+++ src/core/utils/string.ts
@@ -7,8 +7,5 @@ export const getColor = (color: string): string =>
 export const getIconName = (name: string): string =>
   startCase(camelCase(`icon-${name}`)).replace(/\s/g, '');
 
-export const getTextFromHtml = (html: string): string => {
-  const div = document.createElement('div');
-  div.innerHTML = html;
-  return div.textContent || div.innerText || '';
-};
+export const getTextFromHtml = (html: string): string =>
+  html.toString().replace(/(<([^>]+)>)/gi, '');
