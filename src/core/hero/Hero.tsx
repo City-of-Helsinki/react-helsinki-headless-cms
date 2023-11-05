904f9b306e76b616cea297eb0214dc329ad98046
Ivan Melnik <melnik.job@gmail.com>
2023-11-05 13:45:16 +0200

    fix

--- src/core/hero/Hero.tsx
+++ src/core/hero/Hero.tsx
@@ -150,7 +150,7 @@ export default function Hero({
             </div>
           </ContentContainer>
         </Container>
-        {(backgroundColor || imageUrl) && korosType && (
+        {(title || backgroundColor || imageUrl) && korosType && (
           <Koros
             className={classNames(
               styles.heroKoros,
