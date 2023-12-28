0b16276c1c1966bbd3acba10b2442e84ca89c13d
Niko Lindroos <niko.j.makela@gmail.com>
2023-12-28 14:59:45 +0200

    feat: add id, role and size attributes to the tag component
    HH-305 LIIKUNTA-593. 
    There are some values that should be overridable.
    While the HDS Tag-component still has a default value for an ID-property, it needs to overridable and automatically generated, if unset.
    
    The id parameter is already deprecated with default value.
    See more: https://github.com/City-of-Helsinki/helsinki-design-system/blob/master/packages/react/src/components/tag/Tag.tsx#L81).

--- src/common/components/tag/Tag.tsx
+++ src/common/components/tag/Tag.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import classNames from 'classnames';
 import { Tag as HDSTag } from 'hds-react';
+import type { TagProps as HDSTagProps } from 'hds-react';
 
 import styles from './tag.module.scss';
 import { theme1, theme2 } from './tagThemes';
@@ -13,7 +14,7 @@ type Props = {
   whiteOnly?: boolean;
   selected?: boolean;
   onClick?: () => void;
-};
+} & Pick<HDSTagProps, 'id' | 'role' | 'size'>;
 
 export function Tag({
   className,
@@ -22,7 +23,15 @@ export function Tag({
   selected,
   whiteOnly,
   onClick,
+  id,
+  ...hdsTagProps
 }: Props) {
+  /**
+   * @deprecated the generatedId won't be needed after HDS removes the default value from the HDS Tag component:
+   * https://github.com/City-of-Helsinki/helsinki-design-system/blob/master/packages/react/src/components/tag/Tag.tsx#L81).
+   * */
+  const generatedId = React.useId();
+
   const handleClick = (): void => {
     if (onClick) {
       onClick();
@@ -43,6 +52,9 @@ export function Tag({
         !onClick && styles.noOutline,
         className,
       )}
+      {...hdsTagProps}
+      // TODO: don't use the generatedId and allow an undefined id.
+      id={id ?? `tag${generatedId}`}
     >
       {children}
     </HDSTag>
