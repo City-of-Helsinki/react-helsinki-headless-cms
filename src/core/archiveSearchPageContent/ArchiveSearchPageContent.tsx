6a935488a46f6a1b198a7ae222fbf18a00aba00a
Ivan Melnik <melnik.job@gmail.com>
2024-01-08 15:08:52 +0200

    fix

--- src/core/archiveSearchPageContent/ArchiveSearchPageContent.tsx
+++ src/core/archiveSearchPageContent/ArchiveSearchPageContent.tsx
@@ -248,7 +248,7 @@ export function SearchPageContent(props: SearchPageContentProps) {
             {tags && (
               <SearchTags
                 tags={tags}
-                hasClearSearch={Boolean(searchText) && tags.length > 0}
+                hasClearSearch={Boolean(searchText) && currentTags.length > 0}
                 clearAllText={archiveSearch?.clearAll}
                 currentTags={searchTags}
                 handleTagClick={handleTagClick}
