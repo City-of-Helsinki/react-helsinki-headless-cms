9938c4717adede76a3b4453c6229f78196e841a4
Ivan Melnik <melnik.job@gmail.com>
2024-04-03 11:04:50 +0300

    fix link

--- src/core/pageContent/sidebarContent/SidebarCardsList.tsx
+++ src/core/pageContent/sidebarContent/SidebarCardsList.tsx
@@ -4,28 +4,42 @@ import { LayoutCards } from '../../../common/headlessService/types';
 import { SimpleCard } from '../../pageModules/CardsModule/SimpleCard';
 import styles from './sidebarCardsList.module.scss';
 import { useHeadlessCmsLink } from '../../configProvider/useHeadlessCmsLink';
+import { useConfig } from '../../configProvider/useConfig';
+import { getUri } from '../../../common/headlessService/utils';
 
 type CardsListItemProps = Omit<LayoutCards, '__typename'>;
 
 export default function SidebarCardsList({ cards }: CardsListItemProps) {
+  const {
+    internalHrefOrigins,
+    utils: { getIsHrefExternal, getRoutedInternalHref },
+  } = useConfig();
+
   if (cards?.length === 0) {
     return null;
   }
 
   return (
     <div className={styles.cardsContainer}>
-      {cards.map((card) => (
-        <SimpleCard
-          title={card.title}
-          icon={card.icon}
-          description={card.description}
-          backgroundColor={card.backgroundColor}
-          linkUrl={useHeadlessCmsLink(card.link.url)}
-          linkTitle={card.link.title}
-          linkTarget={card.link.target}
-          direction="vertical"
-        />
-      ))}
+      {cards.map((card) => {
+        const uri = getUri(
+          card.link.url,
+          internalHrefOrigins,
+          getIsHrefExternal,
+        );
+        return (
+          <SimpleCard
+            title={card.title}
+            icon={card.icon}
+            description={card.description}
+            backgroundColor={card.backgroundColor}
+            linkUrl={getRoutedInternalHref(uri)}
+            linkTitle={card.link.title}
+            linkTarget={card.link.target}
+            direction="vertical"
+          />
+        );
+      })}
     </div>
   );
 }
