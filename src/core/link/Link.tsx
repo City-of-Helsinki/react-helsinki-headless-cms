94770cc3181326478774153c3f428d2a6e96ed80
Niko Lindroos <niko.j.makela@gmail.com>
2025-09-17 16:53:04 +0300

    fix(types): link and link base
    HCRC-178.

--- src/core/link/Link.tsx
+++ src/core/link/Link.tsx
@@ -1,8 +1,6 @@
-/* eslint-disable react/jsx-no-useless-fragment */
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import classNames from 'classnames';
-import { IconEnvelope, IconPhone } from 'hds-react';
+import { IconEnvelope, IconPhone, IconSize } from 'hds-react';
 
 import LinkBase from './LinkBase';
 import { useConfig } from '../configProvider/useConfig';
@@ -78,7 +76,7 @@ export function Link({
   const isEmail = href?.startsWith('mailto:') || undefined;
   const isPhone = href?.startsWith('tel:') || undefined;
   const isExternal = getIsHrefExternal(href) && !isEmail && !isPhone;
-  const iconSize = size === 'S' ? 'xs' : 's';
+  const iconSize = size === 'S' ? IconSize.ExtraSmall : IconSize.Small;
   const hasImageInLink = findAllElementsOfType(children, ['img']).length > 0;
   // The external links should always open in a new tab.
   const openInNewTab = forceOpenInNewTab ?? isExternal;
