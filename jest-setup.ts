471b2aecf63ed68cb3d1a1de98e530def30b5878
Niko Lindroos <niko.j.makela@gmail.com>
2025-09-17 15:04:13 +0300

    test: remove text decoder global assignment
    HCRC-178

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,8 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@testing-library/jest-dom';
 
-import { TextDecoder, TextEncoder } from 'util';
-
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { toHaveNoViolations } from 'jest-axe';
 // eslint-disable-next-line import/no-extraneous-dependencies
@@ -12,8 +10,3 @@ fetchMock.enableMocks();
 
 // Extend except with jest-axe
 expect.extend(toHaveNoViolations);
-
-// Add for isomorphic-dompurify
-// https://github.com/kkomelin/isomorphic-dompurify/issues/91#issuecomment-1012645198
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
