dde608981492d26d2c4fee37cbac820136a21017
Niko Lindroos <niko.j.makela@gmail.com>
2025-09-22 15:56:43 +0300

    fix(types): languageSorter
    HCRC-178

--- src/core/language/languageSorter.ts
+++ src/core/language/languageSorter.ts
@@ -4,8 +4,12 @@ import type { NonEmptyLanguage } from './types';
 /** Sorting function for NonEmptyLanguage elements. */
 const languageSorter = (left: NonEmptyLanguage, right: NonEmptyLanguage) =>
   Math.sign(
-    LANGUAGE_CODE_ORDINAL_NUMBER[left.code] -
-      LANGUAGE_CODE_ORDINAL_NUMBER[right.code],
+    LANGUAGE_CODE_ORDINAL_NUMBER[
+      left.code as keyof typeof LANGUAGE_CODE_ORDINAL_NUMBER
+    ] -
+      LANGUAGE_CODE_ORDINAL_NUMBER[
+        right.code as keyof typeof LANGUAGE_CODE_ORDINAL_NUMBER
+      ],
   );
 
 export default languageSorter;
