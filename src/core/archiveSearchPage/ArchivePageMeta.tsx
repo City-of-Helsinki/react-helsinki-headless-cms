59c30c77c45e5a4a2b9110bc6f2b947060fcedeb
Niko Lindroos <niko.j.makela@gmail.com>
2025-09-22 12:38:09 +0300

    fix(types): archivePageMeta
    HCRC-178.
    
    Render null when Head-component is not configured. Fix the nullpointer
    exceptions.

--- src/core/archiveSearchPage/ArchivePageMeta.tsx
+++ src/core/archiveSearchPage/ArchivePageMeta.tsx
@@ -18,22 +18,27 @@ function BasicMeta({ title, description }: BasicMetaProps) {
     components: { Head },
     meta,
   } = useConfig();
+  if (!Head) {
+    // eslint-disable-next-line no-console
+    console.warn("No Head component configured. Can't render meta tags.");
+    return null;
+  }
   return (
     <Head>
       {title && <meta property="title" content={title} />}
       {description && <meta property="description" content={description} />}
-      {meta.favIconUrl && <link rel="icon" href={meta.favIconUrl} />}
-      {meta.favIconSvgUrl && (
+      {meta?.favIconUrl && <link rel="icon" href={meta.favIconUrl} />}
+      {meta?.favIconSvgUrl && (
         <link rel="icon" href={meta.favIconSvgUrl} type="image/svg+xml" />
       )}
-      {meta.appleTouchIconUrl && (
+      {meta?.appleTouchIconUrl && (
         <link
           rel="apple-touch-icon"
           sizes="180x180"
           href={meta.appleTouchIconUrl}
         />
       )}
-      {meta.manifestUrl && <link rel="manifest" href={meta.manifestUrl} />}
+      {meta?.manifestUrl && <link rel="manifest" href={meta.manifestUrl} />}
     </Head>
   );
 }
