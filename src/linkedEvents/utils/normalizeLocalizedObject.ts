8d725f30731d9f33af5ac367134a18a407404841
Niko Lindroos <niko.j.makela@gmail.com>
2025-09-23 12:09:53 +0300

    fix(types): normalize localized object
    HCRC-178

--- src/linkedEvents/utils/normalizeLocalizedObject.ts
+++ src/linkedEvents/utils/normalizeLocalizedObject.ts
@@ -16,16 +16,22 @@
  *    }
  * }
  */
-export default <T>(data: T, key: string): T => {
-  const normalizedData = { ...data };
+const normalizeLocalizedObject = <T>(data: T, key: string): T => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const normalizedData: Record<string, any> = {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ...(data as Record<string, any>),
+  };
   normalizedData[key] = {
-    en: normalizedData[`${key}En`],
-    fi: normalizedData[`${key}Fi`],
-    sv: normalizedData[`${key}Sv`],
+    en: normalizedData[`${key}En`] as string,
+    fi: normalizedData[`${key}Fi`] as string,
+    sv: normalizedData[`${key}Sv`] as string,
   };
   delete normalizedData[`${key}En`];
   delete normalizedData[`${key}Fi`];
   delete normalizedData[`${key}Sv`];
 
-  return normalizedData;
+  return normalizedData as T;
 };
+
+export default normalizeLocalizedObject;
