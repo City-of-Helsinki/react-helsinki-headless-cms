d4dc0df903e02a5d17b4f53f8f469f93c37430e8
Niko Lindroos <niko.j.makela@gmail.com>
2025-10-06 13:11:45 +0300

    fix(carousel): add region roles and aria labels to carousel
    HH-380.
    
    To fix or improve carousel component accessibility, the carousel should
    have a role region with a aria-label, that describes the wholeness of
    the component: what is the title of the carousel and how many items does
    it have.
    
    Add region role and aria label to carousel slider. When navigating to
    the carousel slider component, the screen reader should read the current
    slide of the slider, the total amount of items and how many of the items
    are currently shown.

--- src/core/translation/constants.ts
+++ src/core/translation/constants.ts
@@ -1,41 +1,64 @@
 import { logoFi, logoSv } from 'hds-react';
 
+import type { LanguageCodeEnum } from '../../apollo';
+
+type TranslationDictionary = Record<LanguageCodeEnum, string>;
+
 export const FALLBACK_TRANSLATION_KEYS = [
   'headerActionBarLogoAlt',
   'headerActionBarLogoAriaLabel',
   'headerActionBarLogoSrc',
   'headerUniversalBarPrimaryLinkHref',
   'headerUniversalBarPrimaryLinkText',
+  'carouselRegionLabelText',
+  'carouselSliderRegionLabelText',
 ] as const;
 
-export const CITY_OF_HELSINKI_TRANSLATIONS = {
+export const CITY_OF_HELSINKI_TRANSLATIONS: TranslationDictionary = {
   EN: 'City of Helsinki',
   FI: 'Helsingin kaupunki',
   SV: 'Helsingfors stad',
 };
 
-export const HELSINKI_TRANSLATIONS = {
+export const HELSINKI_TRANSLATIONS: TranslationDictionary = {
   EN: 'Helsinki',
   FI: 'Helsinki',
   SV: 'Helsingfors',
 };
 
-export const LOCALIZED_HELSINKI_LOGO_SVG_CONTENTS = {
+export const LOCALIZED_HELSINKI_LOGO_SVG_CONTENTS: TranslationDictionary = {
   EN: logoFi,
   FI: logoFi,
   SV: logoSv,
 };
 
-export const LOCALIZED_HELSINKI_WEBSITE_URL = {
+export const LOCALIZED_HELSINKI_WEBSITE_URL: TranslationDictionary = {
   EN: 'https://www.hel.fi/en',
   FI: 'https://www.hel.fi/fi',
   SV: 'https://www.hel.fi/sv',
 };
 
-export const FALLBACK_TRANSLATIONS = {
+export const CAROUSEL_REGION_LABEL_TEXT: TranslationDictionary = {
+  EN: 'A carousel {title}.',
+  FI: 'Karusellikomponentti {title}.',
+  SV: 'En karusell {title}.',
+};
+
+export const CAROUSEL_SLIDER_REGION_LABEL_TEXT: TranslationDictionary = {
+  EN: 'In carousel slide {currentSlide}. Listing 1 - {itemsPerSlide} items per slide out of {numberOfItems} items.',
+  FI: 'Karusellin sivulla {currentSlide}. Listataan 1 - {itemsPerSlide} korttia kaikkiaan {numberOfItems} kortista.',
+  SV: 'I karusellsidan {currentSlide}. Listar 1 - {itemsPerSlide} objekt per sidan av {numberOfItems} objekt.',
+};
+
+export const FALLBACK_TRANSLATIONS: Record<
+  (typeof FALLBACK_TRANSLATION_KEYS)[number],
+  TranslationDictionary
+> = {
   headerActionBarLogoAlt: HELSINKI_TRANSLATIONS,
   headerActionBarLogoAriaLabel: CITY_OF_HELSINKI_TRANSLATIONS,
   headerActionBarLogoSrc: LOCALIZED_HELSINKI_LOGO_SVG_CONTENTS,
   headerUniversalBarPrimaryLinkHref: LOCALIZED_HELSINKI_WEBSITE_URL,
   headerUniversalBarPrimaryLinkText: CITY_OF_HELSINKI_TRANSLATIONS,
+  carouselRegionLabelText: CAROUSEL_REGION_LABEL_TEXT,
+  carouselSliderRegionLabelText: CAROUSEL_SLIDER_REGION_LABEL_TEXT,
 };
